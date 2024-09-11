7b45aa339e702e8cdd8f885d207ea1a815d8bb6a
Panos Vagenas <35837085+vagenas@users.noreply.github.com>
2024-09-11 10:21:34 +0200

    feat: add hierarchical chunker
    Signed-off-by: Panos Vagenas <35837085+vagenas@users.noreply.github.com>

---
+++ docling_core/chunker/__init__.py
@@ -0,0 +1,8 @@
+#
+# Copyright IBM Corp. 2024 - 2024
+# SPDX-License-Identifier: MIT
+#
+
+"""Define the chunker types."""
+
+from docling_core.chunker.hierarchical_chunker import HierarchicalChunker  # noqa
