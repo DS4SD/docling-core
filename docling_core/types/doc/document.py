17b1419af7f6db2ca29694633ed265bf7005d816
Andrew Tran <48397302+ctandrewtran@users.noreply.github.com>
2024-12-02 11:57:45 -0500

    Add support for xlsx mimetype
    Signed-off-by: Andrew Tran <48397302+ctandrewtran@users.noreply.github.com>

--- docling_core/types/doc/document.py
+++ docling_core/types/doc/document.py
@@ -382,6 +382,7 @@ class DocumentOrigin(BaseModel):
         "application/vnd.openxmlformats-officedocument.presentationml.presentation",
         "text/asciidoc",
         "text/markdown",
+        "application/vnd.openxmlformats-officedocument.spreadsheet.ml.sheet"
     ]
 
     @field_validator("binary_hash", mode="before")
