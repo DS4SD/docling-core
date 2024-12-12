e32f77f0ab1a40213b695c6eff36ea022de493a2
Michele Dolfi <dol@zurich.ibm.com>
2024-12-12 17:17:43 +0100

    add legacy_to_docling_document
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

--- test/test_doc_legacy_convert.py
+++ test/test_doc_legacy_convert.py
@@ -1,7 +1,14 @@
+from pathlib import Path
+
 import yaml
 
 from docling_core.types.doc import DoclingDocument
-from docling_core.utils.legacy import docling_document_to_legacy
+from docling_core.types.doc.base import ImageRefMode
+from docling_core.types.legacy_doc.document import ExportedCCSDocument as DsDocument
+from docling_core.utils.legacy import (
+    docling_document_to_legacy,
+    legacy_to_docling_document,
+)
 
 
 def test_new_to_old():
@@ -13,3 +20,24 @@ def test_new_to_old():
     doc = DoclingDocument.model_validate(dict_from_yaml)
 
     docling_document_to_legacy(doc)
+
+
+def test_old_to_new():
+    # filepath = Path("/Users/dol/Downloads/Santa%20Anna%2C%20Texas.es.json")
+    # filepath = Path("/Users/dol/Downloads/NYSE_COTY_2023.es.json")
+    # filepath = Path("/Users/dol/Downloads/Santa%20Anna%2C%20Texas.es.json")
+
+    test_folder = Path("test/data/legacy_doc")
+    for filepath in test_folder.glob("doc-*.json"):
+        leg_doc = DsDocument.model_validate_json(filepath.read_text())
+
+        doc = legacy_to_docling_document(leg_doc)
+
+        # TODO: validate results
+
+        doc.save_as_yaml(Path(filepath.with_suffix(".new.yaml").name))
+        doc.save_as_markdown(Path(filepath.with_suffix(".new.md").name))
+        doc.save_as_html(
+            Path(filepath.with_suffix(".new.html").name),
+            image_mode=ImageRefMode.EMBEDDED,
+        )
