0e1663f9d7ed1e1f643ff7fc0997ba75d12bfa86
Panos Vagenas <35837085+vagenas@users.noreply.github.com>
2024-12-12 16:22:31 +0100

    feat: add default tokenizer to HybridChunker
    Signed-off-by: Panos Vagenas <35837085+vagenas@users.noreply.github.com>

--- docling_core/transforms/chunker/hybrid_chunker.py
+++ docling_core/transforms/chunker/hybrid_chunker.py
@@ -44,7 +44,9 @@ class HybridChunker(BaseChunker):
 
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
-    tokenizer: Union[PreTrainedTokenizerBase, str]
+    tokenizer: Union[PreTrainedTokenizerBase, str] = (
+        "sentence-transformers/all-MiniLM-L6-v2"
+    )
     max_tokens: int = None  # type: ignore[assignment]
     merge_peers: bool = True
 
