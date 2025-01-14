437a8ca011c411bc087492d162feadbeadd29961
Matteo-Omenetti <Matteo.Omenetti1@ibm.com>
2025-01-14 13:31:59 +0100

    added code item
    Signed-off-by: Matteo-Omenetti <Matteo.Omenetti1@ibm.com>

--- docling_core/types/doc/document.py
+++ docling_core/types/doc/document.py
@@ -1405,7 +1405,7 @@ class DoclingDocument(BaseModel):
     body: GroupItem = GroupItem(name="_root_", self_ref="#/body")  # List[RefItem] = []
 
     groups: List[GroupItem] = []
-    texts: List[Union[SectionHeaderItem, ListItem, TextItem]] = []
+    texts: List[Union[SectionHeaderItem, ListItem, TextItem, CodeItem]] = []
     pictures: List[PictureItem] = []
     tables: List[TableItem] = []
     key_value_items: List[KeyValueItem] = []
