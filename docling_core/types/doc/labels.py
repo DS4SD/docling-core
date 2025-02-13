cbbad6b8fdfdbb40d55e21375581288cf6d1fe1a
Peter Staar <taa@zurich.ibm.com>
2025-02-13 10:45:58 +0100

    added the , and rewrote it to make it more general with a GraphItem, as well as having cell- and link-labels
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- docling_core/types/doc/labels.py
+++ docling_core/types/doc/labels.py
@@ -139,7 +139,25 @@ class TableCellLabel(str, Enum):
         """Get string value."""
         return str(self.value)
 
+class GraphCellLabel(str, Enum):
+    """KeyValueCellLabel"""
 
+    KEY = "key"
+    VALUE = "value"
+
+    CHECKBOX_SELECTED = "checkbox_selected"
+    CHECKBOX_UNSELECTED = "checkbox_unselected"    
+    
+
+class GraphLinkLabel(str, Enum):
+    """KeyValueLinkLabel"""
+
+    TO_VALUE = "to_value"
+    TO_KEY = "to_key"
+
+    TO_PARENT = "to_parent"
+    TO_CHILD = "to_child"
+    
 class CodeLanguageLabel(str, Enum):
     """CodeLanguageLabel."""
 
